5e65e77b10e8dcdcdc6d0096a23a5bf4f7a18379
ChrisVaz1995 <christopher.vazquez2015@gmail.com>
2021-09-14 10:22:10 -0500

    added rollbar

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const path = require('path');
-const { allowedNodeEnvironmentFlags } = require('process');
+// const { allowedNodeEnvironmentFlags } = require('process');
+const Rollbar = requires('rollbar');
+
+let rollbar = new Rollbar({
+    accesstoken: 'b81598868bba40ef96a115d64d852435',
+    captureUncaught: true, 
+    captureUnhandledRejections: true
+})
 
 const app = express();
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-});
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+    rollbar.info('HTML file served sucessfully');
+})
 
 // This is used for Heroku 
 const port = process.env.PORT || 4545;
